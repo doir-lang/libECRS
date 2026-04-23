47923670b10a312497658abf6c1ad529220b587c
Joshua Dahl <joshuadahl@unr.edu>
2026-04-23 10:17:46 -0700

    Variables in Relations

--- relation.hpp
+++ relation.hpp
@@ -3,19 +3,30 @@
 #include "context.hpp"
 #include "storage.hpp"
 #include <span>
+#include <type_traits>
 
 namespace ecrs {
 	struct relation_base {}; // Used for constraints
 
+	struct term {
+		bool is_var;
+		union { entity_t constant; size_t var_id; };
+
+		term() : is_var(true), var_id(0) {}
+		/*implicit*/ term(entity_t e) : is_var(false), constant(e) {}
+		// /*implicit*/ term(var v) : is_var(true), var_id(v.id) {}
+	};
+
 	// Base
 	template<size_t N = std::dynamic_extent, bool CAN_BE_TERM = false>
 	struct relation : public relation_base {
+		using maybe_term = std::conditional_t<CAN_BE_TERM, term, entity_t>;
 		constexpr static bool can_be_term = CAN_BE_TERM;
-		std::array<entity_t, N> related;
+		std::array<maybe_term, N> related;
 
 		constexpr relation() {}
-		constexpr relation(std::array<entity_t, N> a): related(a) {}
-		constexpr relation(std::initializer_list<entity_t> initializer) {
+		constexpr relation(std::array<maybe_term, N> a): related(a) {}
+		constexpr relation(std::initializer_list<maybe_term> initializer) {
 			assert(initializer.size() <= N);
 			auto init = initializer.begin();
 			for(size_t i = 0; i < initializer.size(); ++i, ++init)
@@ -35,12 +46,13 @@ namespace ecrs {
 	template<bool CAN_BE_TERM>
 
 	struct relation<std::dynamic_extent, CAN_BE_TERM> : public relation_base {
+		using maybe_term = std::conditional_t<CAN_BE_TERM, term, entity_t>;
 		constexpr static bool can_be_term = CAN_BE_TERM;
-		std::vector<entity_t> related;
+		std::vector<maybe_term> related;
 
 		relation() {}
-		relation(std::vector<entity_t> a): related(a) {}
-		relation(std::initializer_list<entity_t> e) : related(e) {}
+		relation(std::vector<maybe_term> a): related(a) {}
+		relation(std::initializer_list<maybe_term> e) : related(e) {}
 		relation(relation&&) = default;
 		relation(const relation&) = default;
 		relation& operator=(relation&&) = default;
